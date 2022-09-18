7df6c31b9ca0153bb9add78cfb7c0dd3b9e0ad64
Aevise <111449412+Aevise@users.noreply.github.com>
2022-09-18 19:24:34 +0200

    Add files via upload

--- PY_PasswordManager/PY_PasswordManager/main.py
+++ PY_PasswordManager/PY_PasswordManager/main.py
@@ -3,30 +3,56 @@ import os
 from tkinter import messagebox, simpledialog
 from password_generator import generate_password
 import pyperclip
+import json
+
 
 CANVAS_HEIGHT = 200
 CANVAS_WIDTH = 200
 LOGO = "./logo.png"
 EMAIL = "meteo212@gmail.com"
-FILE = "./data.txt"
+TXT_FILE = "./data.txt"
+JSON_FILE = "./data.json"
 
 # -------------------------------------- methods ---------------------------------
 def save_password()->None:
     """Writes data to the file if all entries are typed in.
     """
-    web_info = website_entry.get()
+    web_info = website_entry.get().capitalize()
     user_info = user_entry.get()
     pass_info = password_entry.get()
+    json_data = {
+        web_info: {
+            "email": user_info,
+            "password": pass_info
+        }
+    }
     if web_info != "" and user_info != "" and pass_info != "":
         decision = messagebox.askokcancel(title = "Entered details", message = f"Entered details:\nWebsite: {web_info}\nEmail/Username: {user_info}\nPassword: {pass_info}\nIs it correct?")
         if decision:
-            with open(FILE, mode = "a") as file:
+            with open(TXT_FILE, mode = "a") as file:
                 file.write(f"{web_info} | {user_info} | {pass_info}\n")
-            messagebox.showinfo(title = "Success", message = "Data entered successfully.")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)  
+            try:
+                with open(JSON_FILE, "r") as file:
+                    #read old data
+                    data = json.load(file)
+                    #update old data
+                    data.update(json_data)
+            except FileNotFoundError:
+                with open(JSON_FILE, "w") as file:
+                    #create new file and dump data
+                    json.dump(json_data, file, indent = 4)
+            else:
+                with open(JSON_FILE, "w") as file:
+                    #save updated data
+                    json.dump(data, file, indent = 4)
+            finally:
+                messagebox.showinfo(title = "Success", message = "Data entered successfully.")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)  
     else:
         messagebox.showinfo(title = "Failure", message = "Please fill in all the entries.")
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
 
 def delete_data()->None:
     """Deletes your saved passwords
@@ -34,10 +60,17 @@ def delete_data()->None:
     decision = messagebox.askokcancel(title = "Data Eraser", message = f"Are you sure you want to remove all your data?\nThis process can not be reversed.")
     if decision:
         try:
-            os.remove(FILE)
-            messagebox.showinfo(title = "Success", message = "Data destroyed.")
+            os.remove(TXT_FILE)
+            messagebox.showinfo(title = "Success", message = "Txt data destroyed.")
         except FileNotFoundError:
-            messagebox.showerror(title = "Failure", message = "No data to destroy.")
+            messagebox.showerror(title = "Failure", message = "No txt data to destroy.")
+        try:
+            os.remove(JSON_FILE)
+            messagebox.showinfo(title = "Success", message = "JSON data destroyed.")
+        except FileNotFoundError:
+            messagebox.showerror(title = "Failure", message = "No JSON data to destroy.")       
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
 
 def random_password()->None:
     """Ask user if he wants to generate a password using standard range of values or if he wants to customize them.
@@ -75,6 +108,24 @@ def random_password()->None:
     pyperclip.copy(new_password)
     messagebox.showinfo(title = "Success", message = "Password successfully generated and copied to your clipboard.")
 
+#-----------------------------------------------------------------------------------------------------------------------------------------
+def search_data():
+    website = website_entry.get().capitalize()
+    if(website != ""):
+        try:
+            with open(JSON_FILE, "r") as file:
+                data = json.load(file)     
+                if(website in data):
+                    data_keys = list(data[website].keys())
+                    messagebox.showinfo(title = f"{website}", message = f"{data_keys[0]}: {data[website][data_keys[0]]}\n{data_keys[1]}: {data[website][data_keys[1]]}")
+                else:
+                    messagebox.showinfo(title = "Error", message = f"No data found regarding {website} website")
+        except FileNotFoundError:
+            messagebox.showerror(title = "Error", message = "No data found.")  
+    else:
+        messagebox.showerror(title = "Error", message = "Please fill in the label first.")
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
 # ------------------------ UI Setup ----------------------------------
 window = Tk()
 window.title("Password Manager")
@@ -96,8 +147,8 @@ password_label = Label(text = "Password:", pady = 5)
 password_label.grid(column = 0, row = 3)
 
     # -------------------------- Entries -------------------------------------
-website_entry = Entry(width = 52)
-website_entry.grid(column = 1, row = 1, columnspan = 2)
+website_entry = Entry(width = 33)
+website_entry.grid(column = 1, row = 1)
 website_entry.focus() #cursor start on this location
 
 user_entry = Entry(width = 52)
@@ -116,5 +167,8 @@ generate_button.grid(column = 2, row = 3)
 
 clear_button = Button(text = "Clear Data", command = delete_data)
 clear_button.grid(column = 2, row = 0)
+
+search_button = Button(text = "Search", highlightthickness = 0, width = 14, command = search_data)
+search_button.grid(column = 2, row = 1)
 
 window.mainloop()
